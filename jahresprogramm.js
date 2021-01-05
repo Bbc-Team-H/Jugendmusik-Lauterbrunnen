bc22c6d50428975ea0c906b5d8162709aff46432
Reinhard Graf <69803246+Dalurei@users.noreply.github.com>
2021-01-05 08:49:27 +0100

    Separation of concerns

---
+++ jahresprogramm.js
@@ -0,0 +1,5 @@
+var months = ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"];
+var n = new Date();
+var y = n.getFullYear();
+var m = n.getMonth();
+document.getElementById("date").innerHTML = months[m] + " " + y;
